c960b72dcc2fb22864907d192f56036316401c36
kamin0 <jm187@yandex.ru>
2022-05-25 22:28:13 +0300

    Добавьте файлы проекта.

---
+++ interface_final/Calculate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace interface_final
+{
+    internal class Summ : ISumm
+    {
+
+        public void Summa(int a, int b)
+        {
+            Console.WriteLine("Сумма чисел равна: {0}", a + b);
+        }
+    }
+}
---
+++ interface_final/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace interface_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Summ calculate = new Summ();
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите первое число:");
+                    var x = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    var y = Convert.ToInt32(Console.ReadLine());
+                    calculate.Summa(x, y);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Должно быть введено число");
+                }
+            }
+        }
+    }
+    public interface ISumm
+    {
+        void Summa(int x, int y);
+    }
+
+}
